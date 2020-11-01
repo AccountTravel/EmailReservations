28853f8fe7b67d71466a492714c76a3a0605bef5
alnidok <alnidok@gmail.com>
2020-11-02 00:50:13 +0300

    Italo - убрал dataProvider из тестов

--- tests/train/ItaloReservationTest.php
+++ tests/train/ItaloReservationTest.php
@@ -6,80 +6,69 @@ use AccountTravel\EmailReservations\Parser;
 
 class ItaloReservationTest extends TrainParserTestCase
 {
-    /**
-     * @dataProvider reservationDataProvider
-     * @param string $fixture
-     * @param array $expected
-     */
-    public function testParseReservation(string $fixture, array $expected)
+    public function testParseReservation()
     {
-        $file = __DIR__ . '/fixtures/' . $fixture;
+        $file = __DIR__ . '/fixtures/Italo-BookingConfirmation.eml';
         $result = (new Parser)->parseFile($file);
 
-        $this->assertEmptyDiff($expected, $result);
+        $this->assertEmptyDiff([
+            [
+                'TrainReservation' => [
+                    'reservationFor' => [
+                        'trainCompany' => 'NTV',
+                        'trainName' => 'Italo',
+                        'trainNumber' => 8924,
+                        'departureTime' => 1567878840,
+                        'arrivalTime' => 1567885320,
+                        'departureStation' => 'Firenze SMN',
+                        'arrivalStation' => 'Venezia ME',
+                    ],
+                    'reservedTicket' => [
+                        'ticketNumber' => 'BFSUKE',
+                        'ticketedSeat' => [
+                            'seatSection' => 5,
+                            'seatNumber' => 5,
+                        ],
+                        'underName' => 'Andrei Dokin',
+                    ],
+
+                    'reservationNumber' => 'BFSUKE',
+                    'reservationStatus' => 'ReservationConfirmed',
+                ]
+            ]
+        ], $result);
     }
 
-    public function reservationDataProvider()
+    public function testParseReservationItalian()
     {
-        return [
-            [
-                'fixture' => 'Italo-BookingConfirmation.eml',
-                'expected' => [
-                    [
-                        'TrainReservation' => [
-                            'reservationFor' => [
-                                'trainCompany' => 'NTV',
-                                'trainName' => 'Italo',
-                                'trainNumber' => 8924,
-                                'departureTime' => 1567878840,
-                                'arrivalTime' => 1567885320,
-                                'departureStation' => 'Firenze SMN',
-                                'arrivalStation' => 'Venezia ME',
-                            ],
-                            'reservedTicket' => [
-                                'ticketNumber' => 'BFSUKE',
-                                'ticketedSeat' => [
-                                    'seatSection' => 5,
-                                    'seatNumber' => 5,
-                                ],
-                                'underName' => 'Andrei Dokin',
-                            ],
+        $file = __DIR__ . '/fixtures/Italo–ConfermaAcquisto.eml';
+        $result = (new Parser)->parseFile($file);
 
-                            'reservationNumber' => 'BFSUKE',
-                            'reservationStatus' => 'ReservationConfirmed',
-                        ]
-                    ]
-                ],
-            ],
+        $this->assertEmptyDiff([
             [
-                'fixture' => 'Italo–ConfermaAcquisto.eml',
-                'expected' => [
-                    [
-                        'TrainReservation' => [
-                            'reservationFor' => [
-                                'trainCompany' => 'NTV',
-                                'trainName' => 'Italo',
-                                'trainNumber' => 8902,
-                                'departureTime' => 1567833300,
-                                'arrivalTime' => 1567838760,
-                                'departureStation' => 'Roma Ter.',
-                                'arrivalStation' => 'Firenze SMN',
-                            ],
-                            'reservedTicket' => [
-                                'ticketNumber' => 'RC45QT',
-                                'ticketedSeat' => [
-                                    'seatSection' => 4,
-                                    'seatNumber' => 9,
-                                ],
-                                'underName' => 'Andrei Dokin',
-                            ],
+                'TrainReservation' => [
+                    'reservationFor' => [
+                        'trainCompany' => 'NTV',
+                        'trainName' => 'Italo',
+                        'trainNumber' => 8902,
+                        'departureTime' => 1567833300,
+                        'arrivalTime' => 1567838760,
+                        'departureStation' => 'Roma Ter.',
+                        'arrivalStation' => 'Firenze SMN',
+                    ],
+                    'reservedTicket' => [
+                        'ticketNumber' => 'RC45QT',
+                        'ticketedSeat' => [
+                            'seatSection' => 4,
+                            'seatNumber' => 9,
+                        ],
+                        'underName' => 'Andrei Dokin',
+                    ],
 
-                            'reservationNumber' => 'RC45QT',
-                            'reservationStatus' => 'ReservationConfirmed',
-                        ]
-                    ]
-                ],
-            ],
-        ];
+                    'reservationNumber' => 'RC45QT',
+                    'reservationStatus' => 'ReservationConfirmed',
+                ]
+            ]
+        ], $result);
     }
 }
